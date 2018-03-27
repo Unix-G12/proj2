27b122c80186f38755bcee2fc3adb33f4254d090
wcj1 <36248206+wcj1@users.noreply.github.com>
2018-03-26 20:43:00 -0500

    Update deleteLine.c

--- deleteLine.c
+++ deleteLine.c
@@ -2,9 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-void deleteLine(int y, int x, int maxx, char arr[][]) {
+void deleteLine(int y, int x, int maxx, int lncount, char arr[][]) {
 	move(y,0);
-	while(0 < maxx) {	
-	memmove(&arr[y][x], &arr[y][x + 1], maxx - x);
-	}
+	int iter = maxx;
+	for(; lncount > y; lncount--) {
+		while(0 < iter) {	
+		memmove(&arr[y][x], &arr[y][x + 1], maxx - x);
+		iter--;
+		}
+	}	
 }	
